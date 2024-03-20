29baf0426c3f7ca8f7258eb6634c46948f8666e9
Fachrihusyaini2EZ <fachrihusyaini2004@gmail.com>
2024-03-20 20:49:16 +0700

    menambahkan autentication dan modul baru beberapa

---
+++ modul/jurnal/index.php
@@ -0,0 +1,47 @@
+<div class="card">
+    <div class="card-header">
+        <h3>Data Jurnal</h3>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Jurnal ID</th>
+                        <th>Pembayaran ID</th>
+                        <th>Pembelian ID</th>
+                        <th>Tanggal Pembayaran</th>
+                        <th>Total Pembayaran</th>
+                        <th>Deskripsi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>2024-03-20</td>
+                        <td>100000</td>
+                        <td>Pembayaran faktur</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>2</td>
+                        <td>2</td>
+                        <td>2024-03-21</td>
+                        <td>50000</td>
+                        <td>Pembayaran tagihan</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>3</td>
+                        <td>3</td>
+                        <td>2024-03-22</td>
+                        <td>75000</td>
+                        <td>Pembayaran hutang</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
