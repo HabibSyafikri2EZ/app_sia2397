1c9e706a3b6940fd282a86be3a27524e569bf02a
FachriHusyaini2EZ <fachrihusyaini2004@gmail.com>
2024-03-27 19:53:44 +0700

    memperbaiki dashboard bagian drop-down

--- dashboard.php
+++ dashboard.php
@@ -1,26 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SIA | Dashboard</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SIA Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="18a105d7cb38e01e5ed0ca255c092992a2e211b39594a7fa57262bfc6fc4ea9c" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
+<style>
+    h3{
+        font-family: georgia;
+    }
+</style>
 <body>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-3 bg-dark text-light" style="min-height:100vh">
-                <div class="text-center">
-                    <i class="bi bi-bank fs-1 text-success"></i>
+            <div class="col-md-3 text-bg-dark" style="min-height:100vh">
+                <div class="text-center pt-4">
+                    <i class="bi bi-cash-coin fs-1 text-success"></i>
                     <h3>Sistem Informasi Akuntansi</h3>
-                    <hr>
-                </div>
+                </div >
+                <div class="pt-4">
                 <?php
                 include_once('sidebar.php');
                 ?>
+                </div>
             </div>
-            <div class="col-md-9">
+            <div class="col-md-9" style="background-color: #F8F6FA">
                 <div class="text-end p-3">
                     <div class="dropdown">
                         <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -29,7 +35,7 @@
                         </button>
                         <ul class="dropdown-menu">
                             <li>
-                                <a class="dropdown-item" href="?modul=profile">
+                                <a class="dropdown-item" href="?modul-profile">
                                     <i class="bi bi-person-badge-fill"></i>
                                     Profile
                                 </a>
@@ -51,6 +57,7 @@
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-+1biHxkHtp56xI78IFeaVFeGpCkqRpob+R2XtSia9d0HNZpJQZWP+O7LNYpvKVg1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </body>
 </html>
